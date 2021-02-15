7efeca1147deef627a4c4a849a383d2f0b3ee975
dachuck17 <78969983+dachuck17@users.noreply.github.com>
2021-02-15 16:47:18 +0100

    Create ejercicioCondicionales.py

---
+++ ejercicioCondicionales.py
@@ -0,0 +1,8 @@
+edad = input('Que edad tienes: ')
+edad = int(edad)
+if edad < 18:
+    print('Pa ti zumito, niñato!')
+elif edad == 18:
+    print('Toma chupito de tequila!')
+else:
+    print('Toma tu birra puto viejo!')
